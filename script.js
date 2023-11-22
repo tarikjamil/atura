d630ecd4ac2d674ff654b76af4517f8a51fb83ad
tarikjamil <tarikjamill@gmail.com>
2023-11-22 15:05:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,13 @@ function pageLoad() {
     ease: "Quint.easeOut",
     duration: 0.5,
   });
+  tl.from("[animation=loading-img]", {
+    scale: 1.2,
+    opacity: "0",
+    stagger: { each: 0.1, from: "start" },
+    ease: "Quint.easeOut",
+    duration: 1,
+  });
   tl.from("[animation=loading]", {
     y: "100%",
     opacity: "0",
