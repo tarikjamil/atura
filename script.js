0835ec0166985c3bc55275a5d044ee83feca0e3c
tarikjamil <tarikjamill@gmail.com>
2024-04-15 10:11:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,93 @@
-  // Register ScrollTrigger plugin
-  gsap.registerPlugin(ScrollTrigger);
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
 
-  // Function to load page
-  function pageLoad() {
-    let tl = gsap.timeline();
-    tl.to(".main-wrapper", {
-      opacity: 1,
-      ease: "Quint.easeOut",
-      duration: 0.5,
-    });
-    tl.from("[animation=loading-img]", {
-      scale: 1.2,
-      opacity: "0",
-      stagger: { each: 0.1, from: "start" },
-      ease: "Quint.easeOut",
-      duration: 1,
-    });
-    tl.from("[animation=loading]", {
-      y: "100%",
-      opacity: "0",
-      stagger: { each: 0.1, from: "start" },
-      ease: "Quint.easeOut",
-      duration: 1,
-    });
-  }
-  // Run the page load function
-  pageLoad();
+// Function to load page
+function pageLoad() {
+  let tl = gsap.timeline();
+  tl.to(".main-wrapper", {
+    opacity: 1,
+    ease: "Quint.easeOut",
+    duration: 0.5,
+  });
+  tl.from("[animation=loading-img]", {
+    scale: 1.2,
+    opacity: "0",
+    stagger: { each: 0.1, from: "start" },
+    ease: "Quint.easeOut",
+    duration: 1,
+  });
+  tl.from("[animation=loading]", {
+    y: "100%",
+    opacity: "0",
+    stagger: { each: 0.1, from: "start" },
+    ease: "Quint.easeOut",
+    duration: 1,
+  });
+}
+// Run the page load function
+pageLoad();
 
-  // Initialize Splide slider when DOM is loaded
-  document.addEventListener("DOMContentLoaded", function () {
-    var splide = new Splide(".is--splide-slider", {
-      type: "slider",
-      perPage: 3,
-      perMove: 1,
-      pagination: false,
-      arrows: false,
-      gap: "26rem",
-      breakpoints: {
-        991: {
-          perPage: 1,
-          gap: "24rem",
-        },
+// Initialize Splide slider when DOM is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".is--splide-slider", {
+    type: "slider",
+    perPage: 3,
+    perMove: 1,
+    pagination: false,
+    arrows: false,
+    gap: "26rem",
+    breakpoints: {
+      991: {
+        perPage: 1,
+        gap: "24rem",
+      },
+    },
+  });
+  splide.mount();
+});
+
+// Select all SVGs
+const svgs = document.querySelectorAll(".svg--top, .svg--bottom");
+
+// Loop through each SVG
+svgs.forEach((svg) => {
+  const paths = svg.querySelectorAll("path");
+  const totalLength = Array.from(paths).reduce(
+    (total, path) => total + path.getTotalLength(),
+    0
+  );
+
+  let cumulativeLength = 0;
+
+  // Loop through each path in the SVG
+  paths.forEach((path) => {
+    const pathLength = path.getTotalLength();
+    const start = (cumulativeLength / totalLength) * 100;
+    const end = ((cumulativeLength + pathLength) / totalLength) * 100;
+
+    // Set initial stroke properties
+    gsap.set(path, {
+      strokeDasharray: pathLength,
+      strokeDashoffset: pathLength > 0 ? pathLength : 0,
+    });
+
+    // Animate the stroke offset
+    gsap.to(path, {
+      strokeDashoffset: 0,
+      scrollTrigger: {
+        trigger: svg,
+        scrub: true,
+        start: "top center",
+        end: "bottom center",
+        markers: false,
       },
     });
-    splide.mount();
+
+    cumulativeLength += pathLength > 0 ? pathLength : 0;
   });
+});
 
-  // Select all SVGs
-  const svgs = document.querySelectorAll(".svg--top, .svg--bottom");
-
-  // Loop through each SVG
-  svgs.forEach((svg) => {
-    const paths = svg.querySelectorAll("path");
-    const totalLength = Array.from(paths).reduce(
-      (total, path) => total + path.getTotalLength(),
-      0
-    );
-
-    let cumulativeLength = 0;
-
-    // Loop through each path in the SVG
-    paths.forEach((path) => {
-      const pathLength = path.getTotalLength();
-      const start = (cumulativeLength / totalLength) * 100;
-      const end = ((cumulativeLength + pathLength) / totalLength) * 100;
-
-      // Set initial stroke properties
-      gsap.set(path, {
-        strokeDasharray: pathLength,
-        strokeDashoffset: pathLength > 0 ? pathLength : 0,
-      });
-
-      // Animate the stroke offset
-      gsap.to(path, {
-        strokeDashoffset: 0,
-        scrollTrigger: {
-          trigger: svg,
-          scrub: true,
-          start: "top center",
-          end: "bottom center",
-          markers: false,
-        },
-      });
-
-      cumulativeLength += pathLength > 0 ? pathLength : 0;
-    });
-  });
-
-  // Add click event listener to navlink and background menu
-  $(document).on("click", ".navlink, .background--menu", function () {
-    $(".close--btn").click();
-  });
-})();
+// Add click event listener to navlink and background menu
+$(document).on("click", ".navlink, .background--menu", function () {
+  $(".close--btn").click();
+});
